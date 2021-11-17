83af52e61ca32cd45634ee0aefd06305442e82b6
AdarshMishraji <adarshmishra969@gmail.com>
2021-11-17 22:46:54 +0530

    old optimzed

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://shorty--urls-server.herokuapp.com/";
+export const BASE_URL = "https://shorty-urls-server-old.vercel.app/";
 // export const BASE_URL = "http://localhost:4000/";
 export const Authorization = "05d5f47ab1314523bffef0e918afd3cb";
